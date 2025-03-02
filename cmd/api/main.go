59e172a4d9adcb6c5d83f2e9387ede935e6b7279
Hubert Dudziński <hdudzinski.kontakt@gmail.com>
2025-03-02 19:33:11 +0100

    Moved starting server from server.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,24 @@
 package main
 
-import "github.com/HDudz/SWIFT-Parser/internal/server"
+import (
+	"fmt"
+	"github.com/HDudz/SWIFT-Parser/internal/server"
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+)
 
 func main() {
+
+	r := chi.NewRouter()
+
 	db := server.ConnectDB()
-	server.ImportData(db)
-	server.StartServer()
+
+	server.ImportDataIfNeeded(db)
+
+	r.Get("/swift/{code}", server.GetCodeHandler(db))
+
+	port := "8080"
+	fmt.Printf("Server running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
