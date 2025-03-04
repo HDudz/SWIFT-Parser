f96050cac6e472c9f43d6a0d0c8710ab56c9e1f5
Hubert Dudziński <hdudzinski.kontakt@gmail.com>
2025-03-04 17:04:44 +0100

    Changed models names to be more clear.

--- internal/models/swiftCode.go
+++ internal/models/swiftCode.go
@@ -1,18 +1,18 @@
 package models
 
-type SubModel struct {
+type SubSwift struct {
 	Address     string `json:"address"`
 	BankName    string `json:"bankName"`
 	CountryISO2 string `json:"countryISO2"`
 	IsHQ        bool   `json:"isHeadquarter"`
 	Code        string `json:"swiftCode"`
 }
-type MainModel struct {
+type MainSwift struct {
 	Address     string      `json:"address"`
 	BankName    string      `json:"bankName"`
 	CountryISO2 string      `json:"countryISO2"`
 	CountryName string      `json:"countryName"`
 	IsHQ        bool        `json:"isHeadquarter"`
 	Code        string      `json:"swiftCode"`
-	Branches    *[]SubModel `json:"branches,omitempty"`
+	Branches    *[]SubSwift `json:"branches,omitempty"`
 }
