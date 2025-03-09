7f262a41fae9f9a476e1af4ef7d2158992a71b4d
Hubert Dudziński <hdudzinski.kontakt@gmail.com>
2025-03-09 16:56:27 +0100

    Unit tests for import functions

---
+++ test/importer_test.go
@@ -0,0 +1,85 @@
+package test
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/HDudz/SWIFT-Parser/internal/database"
+	"os"
+	"regexp"
+	"testing"
+)
+
+func TestImportFromCSV(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		panic("Error when creating sqlmock: " + err.Error())
+	}
+	defer db.Close()
+
+	tmpFile, err := os.CreateTemp(os.TempDir(), "test_swift.csv")
+	if err != nil {
+		t.Fatalf("Nie udało się stworzyć pliku tymczasowego: %v", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	testData := `COUNTRY ISO2 CODE,SWIFT CODE,CODE TYPE,NAME,ADDRESS,TOWN NAME,COUNTRY NAME,TIME ZONE
+PL,ABCDEFGH,BIC11,Test Bank,"Test Address",Warsaw,POLAND,Europe/Warsaw`
+	if _, err := tmpFile.WriteString(testData); err != nil {
+		t.Fatalf("Błąd zapisu do pliku tymczasowego: %v", err)
+	}
+	tmpFile.Close()
+
+	mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO swiftTable (country_iso2, code, bank_name, address, country_name, is_hq) 
+            VALUES (?, ?, ?, ?, ?, ?)`)).
+		WithArgs("PL", "ABCDEFGH", "Test Bank", "Test Address", "POLAND", false).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err = database.ImportFromCSV(db, tmpFile.Name())
+	if err != nil {
+		t.Errorf("ImportFromCSV zwróciło błąd: %v", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Nie spełniono oczekiwań SQL mocka: %v", err)
+	}
+}
+
+func TestImportDataIfNeeded(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		panic("Error when creating sqlmock: " + err.Error())
+	}
+	defer db.Close()
+
+	tmpFile, err := os.CreateTemp(os.TempDir(), "test_swift.csv")
+	if err != nil {
+		t.Fatalf("Nie udało się stworzyć pliku tymczasowego: %v", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	testData := `COUNTRY ISO2 CODE,SWIFT CODE,CODE TYPE,NAME,ADDRESS,TOWN NAME,COUNTRY NAME,TIME ZONE
+PL,ABCDEFGH,BIC11,Test Bank,"Test Address",Warsaw,POLAND,Europe/Warsaw`
+
+	if _, err := tmpFile.WriteString(testData); err != nil {
+		t.Fatalf("Błąd zapisu do pliku tymczasowego: %v", err)
+	}
+	tmpFile.Close()
+
+	rowsMain := sqlmock.NewRows([]string{"count"}).AddRow(0)
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT COUNT(*) FROM swiftTable`)).WillReturnRows(rowsMain)
+
+	mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO swiftTable (country_iso2, code, bank_name, address, country_name, is_hq) 
+            VALUES (?, ?, ?, ?, ?, ?)`)).
+		WithArgs("PL", "ABCDEFGH", "Test Bank", "Test Address", "POLAND", false).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err = database.ImportDataIfNeeded(db, tmpFile.Name())
+	if err != nil {
+		t.Errorf("ImportDataIfNeeded zwróciło błąd: %v", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Nie spełniono oczekiwań SQL mocka: %v", err)
+	}
+}
