df7dd3517244f4ef1bfe3fe19b24be4b9b28848e
Hubert Dudziński <hdudzinski.kontakt@gmail.com>
2025-03-12 14:37:52 +0100

    Added checking if record we want to delete was found.

--- internal/handlers/delete_code.go
+++ internal/handlers/delete_code.go
@@ -12,8 +12,23 @@ func DeleteCodeHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		code := chi.URLParam(r, "swift-code")
+		var howManyFound int
 
-		_, err := db.Exec(`DELETE FROM swiftTable WHERE code = ?`, code)
+		err := db.QueryRow(`SELECT COUNT(*) FROM swiftTable WHERE code = ?`, code).Scan(&howManyFound)
+
+		if err != nil {
+			log.Printf("Database error: %v", err)
+			http.Error(w, "Failed to find code", http.StatusNotFound)
+			return
+		}
+
+		if howManyFound < 1 {
+			log.Printf("Code not found")
+			http.Error(w, "Code not found", http.StatusNotFound)
+			return
+		}
+
+		_, err = db.Exec(`DELETE FROM swiftTable WHERE code = ?`, code)
 
 		if err != nil {
 			log.Printf("Database error: %v", err)
@@ -22,7 +37,7 @@ func DeleteCodeHandler(db *sql.DB) http.HandlerFunc {
 		}
 
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusNoContent)
+		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(map[string]string{"message": "Swift code deleted successfully"})
 
 	}
