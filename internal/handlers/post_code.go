414909991145a37728acba0faa5f95de90678550
Hubert Dudziński <hdudzinski.kontakt@gmail.com>
2025-03-13 18:25:30 +0100

    Added better data validation to post handler.

--- internal/handlers/post_code.go
+++ internal/handlers/post_code.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"github.com/HDudz/SWIFT-Parser/internal/models"
 	"log"
 	"net/http"
@@ -11,10 +12,48 @@ import (
 
 func PostCodeHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		var rawData map[string]json.RawMessage
+
+		if err := json.NewDecoder(r.Body).Decode(&rawData); err != nil {
+			http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+			return
+		}
+
+		requiredFields := []string{"address", "bankName", "countryISO2", "countryName", "isHeadquarter", "swiftCode"}
+
+		for _, field := range requiredFields {
+			_, exists := rawData[field]
+			if !exists {
+				http.Error(w, fmt.Sprintf("Missing required field: %s", field), http.StatusBadRequest)
+				return
+			}
+		}
+
 		var newCode models.MainSwift
 
-		if err := json.NewDecoder(r.Body).Decode(&newCode); err != nil {
-			http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		if err := json.Unmarshal(rawData["address"], &newCode.Address); err != nil || newCode.Address == "" {
+			http.Error(w, "Invalid or missing 'address'", http.StatusBadRequest)
+			return
+		}
+		if err := json.Unmarshal(rawData["bankName"], &newCode.BankName); err != nil || newCode.BankName == "" {
+			http.Error(w, "Invalid or missing 'bankName'", http.StatusBadRequest)
+			return
+		}
+		if err := json.Unmarshal(rawData["countryISO2"], &newCode.CountryISO2); err != nil || newCode.CountryISO2 == "" {
+			http.Error(w, "Invalid or missing 'countryISO2'", http.StatusBadRequest)
+			return
+		}
+		if err := json.Unmarshal(rawData["countryName"], &newCode.CountryName); err != nil || newCode.CountryName == "" {
+			http.Error(w, "Invalid or missing 'countryName'", http.StatusBadRequest)
+			return
+		}
+		if err := json.Unmarshal(rawData["swiftCode"], &newCode.Code); err != nil || newCode.Code == "" {
+			http.Error(w, "Invalid or missing 'swiftCode'", http.StatusBadRequest)
+			return
+		}
+		if err := json.Unmarshal(rawData["isHeadquarter"], &newCode.IsHQ); err != nil {
+			http.Error(w, "Invalid 'isHeadquarter' field, must be true or false", http.StatusBadRequest)
 			return
 		}
 
